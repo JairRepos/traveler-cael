b3360c8bb46f9b7ed69a251ad4cff18e33fb4d9a
JairCibertec <cuentafeik7x@gmail.com>
2022-06-08 19:05:30 -0500

    Landing Traveler

---
+++ main.js
@@ -0,0 +1,52 @@
+AOS.init();
+
+// Burger menus
+document.addEventListener('DOMContentLoaded', function () {
+    // open
+    const burger = document.querySelectorAll('.navbar-burger');
+    const menu = document.querySelectorAll('.navbar-menu');
+    const optionNav = document.querySelectorAll('.option-nav');
+
+    optionNav.forEach(option => {
+        option.addEventListener('click', function () {
+            menu[0].classList.add('hidden');
+        })
+    });
+
+    console.log(burger.length);
+    console.log(menu.length);
+
+    if (burger.length && menu.length) {
+        for (var i = 0; i < burger.length; i++) {
+            burger[i].addEventListener('click', function () {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    // close
+    const close = document.querySelectorAll('.navbar-close');
+    const backdrop = document.querySelectorAll('.navbar-backdrop');
+
+    if (close.length) {
+        for (var i = 0; i < close.length; i++) {
+            close[i].addEventListener('click', function () {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+
+    if (backdrop.length) {
+        for (var i = 0; i < backdrop.length; i++) {
+            backdrop[i].addEventListener('click', function () {
+                for (var j = 0; j < menu.length; j++) {
+                    menu[j].classList.toggle('hidden');
+                }
+            });
+        }
+    }
+});
